c72f09c5ab14bd2ba63f6546cffb2c48e019c5ef
ShengRang <541628498@qq.com>
2015-12-11 23:07:38 +0800

    抄的session太辣鸡, 回头重构

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 # -*- coding:utf-8 -*-
 
 import os
+import time
 
 import tornado.ioloop
 import tornado.web
+import ujson
+
+import session
+
+class BaseHandler(tornado.web.RequestHandler):
+    def __init__(self, *argc, **argkw):
+        super(BaseHandler, self).__init__(*argc, **argkw)
+        self.session = session.Session(self.application.session_manager, self)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write('hello world')
 
 
-class CaptchaHandler(tornado.web.RequestHandler):
+class CaptchaHandler(BaseHandler):
     def get(self):
+        self.session['test'] = time.strftime('%Y-%m-%d %H:%M:%S')
+        self.session.save()
         self.render('captcha.html', **{
             'title': u'验证码',
-            'msg': u'欢迎使用Simple-Captcha'
+            'msg': u'欢迎使用Simple-Captcha',
+            'data': ujson.dumps(self.session)
         })
 
 
@@ -30,9 +42,18 @@ class Application(tornado.web.Application):
             template_path = os.path.join(os.path.dirname(__file__), "templates"),
             #静态文件路径
             static_path = os.path.join(os.path.dirname(__file__), "static"),
+            cookie_secret = "e446976943b4e8442f099fed1f3fea28462d5832f483a0ed9a3d5d3859f==78d",
+            session_secret = "3cdcb1f00803b6e78ab50b466a40b9977db396840c28307f428b25e2277f1bcc",
+            session_timeout = 60,
+            store_options = {
+                'redis_host': 'localhost',
+                'redis_port': 6379,
+                'redis_pass': '',
+            },
         )
-
         tornado.web.Application.__init__(self, handlers, **settings)
+        self.session_manager = session.SessionManager(settings["session_secret"]
+        , settings["store_options"], settings["session_timeout"])
 
 
 if __name__ == '__main__':
--- session.py
+++ session.py
@@ -7,6 +7,7 @@ import hmac
 import ujson
 import redis
 
+
 class SessionData(dict):
     def __init__(self, session_id, hmac_key):
         self.session_id, self.hmac_key = session_id, hmac_key
@@ -17,12 +18,18 @@ class Session(SessionData):
         self.session_manager = session_manager
         self.request_handler = request_handler
         # try to get current session if exist
-        pass
+        current_data = session_manager.get(request_handler)
+        self.session_id, self.hmac_key = current_data.session_id, current_data.hmac_key
+        self.update(current_data)
+
+    def save(self):
+        self.session_manager.set(self.request_handler, self)
 
 
 class SessionManager(object):
     def __init__(self, secret, store_options, session_timeout):
         self.secret, self.store_options = secret, store_options
+        self.session_timeout = session_timeout
         self.redis = redis.StrictRedis(host=store_options.get('redis_host')
         , port=store_options.get('redis_port')
         , password=store_options.get('redis_pwd'))
@@ -30,13 +37,11 @@ class SessionManager(object):
     def _fetch(self, session_id):
         raw_data = self.redis.get(session_id)
         data = {}
+        session_data = {}
         if raw_data:
             self.redis.expire(session_id, self.session_timeout)
             session_data = ujson.loads(raw_data)
-        if isinstance(session_data, dict):
-            return session_data
-        else:
-            return {}
+        return session_data
 
     def get(self, request_handler = None):
         session_id, hmac_key = None, None
@@ -47,10 +52,11 @@ class SessionManager(object):
         if not session_id:
             exists = False
             session_id = self._generate_id()
-            hmac_key = self._generate_hmac()
+            hmac_key = self._generate_hmac(session_id)
         check_hmac = self._generate_hmac(session_id)
         if hmac_key != check_hmac:
-            raise InvalidSessionException()
+            #raise InvalidSessionException()
+            return SessionData(session_id, check_hmac)
         session = SessionData(session_id, hmac_key)
         if exists:
             session_data = self._fetch(session_id)
@@ -58,13 +64,16 @@ class SessionManager(object):
         return session
 
     def set(self, request_handler, session):
-        pass
+        request_handler.set_secure_cookie("session_id", session.session_id)
+        request_handler.set_secure_cookie("verification", session.hmac_key)
+        session_data = ujson.dumps(session)
+        self.redis.setex(session.session_id, self.session_timeout, session_data)
 
-    def _generate_hmac(self):
-        pass
+    def _generate_hmac(self, session_id):
+        return hmac.new(session_id, self.secret, hashlib.sha256).hexdigest()
 
     def _generate_id(self):
-        pass
+        return hashlib.sha256(self.secret + str(uuid.uuid4())).hexdigest()
 
 
 class InvalidSessionException(Exception):
